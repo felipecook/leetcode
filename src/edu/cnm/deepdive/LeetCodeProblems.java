d4312e58c4b0250b5881978076c16f04d827dfc1
Felipe Cook <felipepcook@gmail.com>
2019-03-13 19:54:17 -0600

    completed centeredAverage on coding bat

--- src/edu/cnm/deepdive/LeetCodeProblems.java
+++ src/edu/cnm/deepdive/LeetCodeProblems.java
@@ -1,23 +1,30 @@
 package edu.cnm.deepdive;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LeetCodeProblems {
 
   public static int maxSpan(int[] nums) {
-    int tempCounter = 0;
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = 0; j < nums.length; j++) {
-        int counter = 0;
-        if (nums[i] == nums[j]) {
-          counter++;
-        }
-        if (counter > tempCounter) {
-          tempCounter = counter;
-        }
-      }
+
+    Arrays.sort(nums);
+
+    int [] removedFirstAndLastNums =
+        Arrays.copyOfRange(nums, 1, nums.length - 1);
+    int total = 0;
+    for (int i = 0; i < removedFirstAndLastNums.length - 1; i++) {
+      total += removedFirstAndLastNums[i];
+
     }
+    int average = 0;
+
+    average = total / removedFirstAndLastNums.length;
+
+    return average;
+
+
+
   }
 }
 
--- test/edu/cnm/deepdive/LeetCodeProblemsTest.java
+++ test/edu/cnm/deepdive/LeetCodeProblemsTest.java
@@ -9,14 +9,14 @@ class LeetCodeProblemsTest {
 
 
   @Test
-  void getMiddleEvenTests() {
-    assertEquals("es", LeetCodeProblems.getMiddle("test"));
-    assertEquals("dd", LeetCodeProblems.getMiddle("middle"));
+  void maxSpan() {
+
+    int[] test = {1, 2, 3, 4, 100};
+    int[] test1 = {1, 1, 5, 5, 10, 8, 7};
+
+    assertEquals(3, LeetCodeProblems.maxSpan(test));
+    assertEquals(5, LeetCodeProblems.maxSpan(test1));
   }
 
-  @Test
-  void getMiddleOddTests() {
-    assertEquals("t", LeetCodeProblems.getMiddle("testing"));
-    assertEquals("A", LeetCodeProblems.getMiddle("A"));
-  }
+
 }
