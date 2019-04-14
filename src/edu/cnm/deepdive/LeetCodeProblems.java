f91654b67f6ddef3917a5e120252159c0a8bdb90
Felipe Cook <felipepcook@gmail.com>
2019-04-14 16:30:28 -0600

    Solved fizzArray on CodingBat

--- src/edu/cnm/deepdive/LeetCodeProblems.java
+++ src/edu/cnm/deepdive/LeetCodeProblems.java
@@ -2,30 +2,30 @@ package edu.cnm.deepdive;
 
 public class LeetCodeProblems {
 
-    public static boolean isPalindrome (int number) {
-      int reverse = 0;
-      int numberToBeReversed = number;
+    public static int[] fizzArray(int n) {
 
-      while (numberToBeReversed != 0) {
-        int lastDigit = number % 10;
-        reverse = (reverse * 10) + lastDigit;
-        numberToBeReversed /= 10;
-      }
+        int[] holderArray = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            holderArray[i] += i;
+
+        }
+
+        return holderArray;
 
 
-      if (number == reverse) {
-        return true;
-      } else  {
-        return false;
-      }
+
     }
 
-  public static void main(String[] args) {
-    isPalindrome(-222);
+    public static void main(String[] args) {
+        System.out.println(fizzArray(10).toString());
 
-  }
+
+    }
 
 }
+
+
 
 
 
