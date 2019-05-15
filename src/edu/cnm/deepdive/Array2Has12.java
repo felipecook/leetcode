4c48eb486f1a3c88e308e186958422a020c983b0
Felipe Cook <felipepcook@gmail.com>
2019-05-14 20:40:34 -0700

    Added variables to track array position

--- src/edu/cnm/deepdive/Array2Has12.java
+++ src/edu/cnm/deepdive/Array2Has12.java
@@ -5,16 +5,23 @@ public class Array2Has12 {
   public boolean has12(int[] nums) {
     boolean marker1 = false;
     boolean marker2 = false;
+    int val1 = 0;
+    int val2 = 0;
+
 
     for (int i = 0; i < nums.length; i++) {
+
       if (nums[i] == 1) {
         marker1 = true;
+        val1 = i;
       }
       if (nums[i] == 2) {
         marker2 = true;
+        val2 = 1;
       }
     }
-    return marker1 && marker2;
+    return ((val1 < val2) && (marker1 && marker2));
+
   }
 
 }
