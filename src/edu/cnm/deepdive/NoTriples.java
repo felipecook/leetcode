ab2f1239fcfeba05273a905d282350cc50a10969
Felipe Cook <felipepcook@gmail.com>
2019-05-14 20:04:13 -0700

    completed problem

--- src/edu/cnm/deepdive/NoTriples.java
+++ src/edu/cnm/deepdive/NoTriples.java
@@ -3,17 +3,18 @@ package edu.cnm.deepdive;
 public class NoTriples {
 
   public boolean noTriples(int[] nums) {
-    int val = 0;
 
     for (int i = 0; i < (nums.length - 2); i++) {
-      val = nums[i];
+      int val = nums[i];
 
 
-      return val == nums[i + 1] && val == nums[i + 2];
+      if ((val == nums[i + 1]) && (val == nums[i + 2])) {
+        return false;
+      }
 
     }
 
-    return false;
+    return true;
 
 
   }
--- test/edu/cnm/deepdive/NoTriplesTest.java
+++ test/edu/cnm/deepdive/NoTriplesTest.java
@@ -9,9 +9,12 @@ class NoTriplesTest {
   NoTriples noTriples = new NoTriples();
 
   int[] array1 = {1, 1, 2, 2, 1};
+  int[] array2 = {1, 1, 2, 2, 2, 1};
 
   @Test
   void noTriples() {
     assertEquals(true, noTriples.noTriples(array1));
+    assertEquals(false, noTriples.noTriples(array2));
+
   }
 }
