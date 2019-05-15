15831199f6c4803788bd9ecf28f701b43d3ae21b
Felipe Cook <felipepcook@gmail.com>
2019-05-14 20:11:34 -0700

    Initial commit on Has77, solved more than half of tests

---
+++ src/edu/cnm/deepdive/Array2Has77.java
@@ -0,0 +1,18 @@
+package edu.cnm.deepdive;
+
+public class Array2Has77 {
+
+  public boolean has77(int[] nums) {
+
+    for (int i = 0; i < nums.length - 2; i++) {
+      int val = nums[i];
+      if ((val == 7) && (val == nums[i + 1] || (val == nums[i + 2]))) {
+        return true;
+      }
+    }
+    return false;
+
+  }
+
+
+}
