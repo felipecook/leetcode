4d57f12107a891b845a0dbb342bc4315d5cf66a0
Felipe Cook <felipepcook@gmail.com>
2019-05-15 22:04:25 -0700

    Solved using REGEX solution

--- src/edu/cnm/deepdive/String2DoubleChar.java
+++ src/edu/cnm/deepdive/String2DoubleChar.java
@@ -4,12 +4,15 @@ public class String2DoubleChar {
 
   public String doubleChar(String str) {
 
-    String returnString = "";
-
-    for (int i = 0; i < str.length(); i++) {
-      returnString += str.charAt(i) + str.charAt(i);
-    }
-    return returnString;
+    String s=str.replaceAll(".", "$0$0");
+    return s;
+//    String returnString = "";
+//
+//
+//    for (int i = 0; i < str.length(); i++) {
+//      returnString += str.charAt(i) + str.charAt(i);
+//    }
+//    return returnString;
 
   }
 
