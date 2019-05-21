4b41ff7a1b0ca93154cf748c6e43ad4aaf0dda7a
Felipe Cook <felipepcook@gmail.com>
2019-05-20 20:06:10 -0700

    Implemented 2 for loops to cycle through the words and the letters

--- src/edu/cnm/deepdive/String3CountYZ.java
+++ src/edu/cnm/deepdive/String3CountYZ.java
@@ -3,9 +3,27 @@ package edu.cnm.deepdive;
 public class String3CountYZ {
 
   public static int countYZ(String str) {
-    String[] arr = str.split(" ");
+    // Counter to return number of instances Y and Z appear at the end of the word.
+    int counter = 0;
 
-    
+    // Split the sentence string into multiple words.
+    String[] strArr = str.split(" ");
+
+    for (int i = 0; i < strArr.length; i++) {
+      // This grabs each word in the string array
+      String testWord = strArr[i];
+      for (int j = 0; j < testWord.length(); j++) {
+        char testCharacter = testWord.charAt(j);
+        if (testCharacter == 'y' || testCharacter == 'z') {
+          counter++;
+        }
+      }
+    }
+
+
+
+
+
   }
 
 
