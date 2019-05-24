10da319f7d65c03e2ade5a550ea01935b11bb7d0
Felipe Cook <felipepcook@gmail.com>
2019-05-23 19:47:20 -0700

    Attempt; needs return still.

--- src/edu/cnm/deepdive/String3WithoutString.java
+++ src/edu/cnm/deepdive/String3WithoutString.java
@@ -1,9 +1,33 @@
 package edu.cnm.deepdive;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class String3WithoutString {
 
   public String withoutString(String base, String remove) {
+
+    // Array list to hold converted base string
+    ArrayList<Character> baseToList = new ArrayList<Character>();
+
+    // Adds elements of base string to Array List
+    for (char c: base.toCharArray()) {
+      baseToList.add(c);
+    }
+
+    // Iterate over the elements in the list
+    for (int i = 0; i < baseToList.size(); i++) {
+
+      // If an element in the list is similar to the String, remove it
+      if (baseToList.get(i).equals(remove)) {
+        baseToList.remove(i);
+      }
+
+
+    }
+
     
+
   }
 
 }
