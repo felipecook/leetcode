f9da189382f90dbaf3838829a616942acda7cfb5
Felipe Cook <felipepcook@gmail.com>
2019-05-23 19:59:18 -0700

    This regex solution solves more than half of tests

--- src/edu/cnm/deepdive/String3WithoutString.java
+++ src/edu/cnm/deepdive/String3WithoutString.java
@@ -3,30 +3,36 @@ package edu.cnm.deepdive;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+Given two strings, base and remove, return a version of the base string
+where all instances of the remove string have been removed (not case sensitive).
+You may assume that the remove string is length 1 or more.
+Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+ */
+
 public class String3WithoutString {
+
 
   public String withoutString(String base, String remove) {
 
-    // Array list to hold converted base string
-    ArrayList<Character> baseToList = new ArrayList<Character>();
-
-    // Adds elements of base string to Array List
-    for (char c: base.toCharArray()) {
-      baseToList.add(c);
-    }
-
-    // Iterate over the elements in the list
-    for (int i = 0; i < baseToList.size(); i++) {
-
-      // If an element in the list is similar to the String, remove it
-      if (baseToList.get(i).equals(remove)) {
-        baseToList.remove(i);
-      }
+    return base.replaceAll(remove, "");
 
 
-    }
 
-    
+
+//    if (remove.length() > base.length()) {
+//      return null;
+//    }
+//    // Create a substring of the length of remove from base and iterate up through base with that substring.
+//
+//    for (int i = 0; i < base.length() - remove.length(); i++) {
+//      String testString = base.substring(i, remove.length());
+//
+//      // if that substring equals the remove string, remove that substring from the string
+//      if (testString.equals(remove)) {
+//
+//      }
+//    }
 
   }
 
