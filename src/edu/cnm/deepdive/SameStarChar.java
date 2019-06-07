7cba8a5490fd5d0ac417ca12cbda1f8c6ba2600e
Felipe Cook <felipepcook@gmail.com>
2019-06-06 22:14:04 -0700

    Initial commit on SameStarChar

---
+++ src/edu/cnm/deepdive/SameStarChar.java
@@ -0,0 +1,22 @@
+package edu.cnm.deepdive;
+
+
+/*
+Returns true if for every '*' (star) in the string, if
+there are chars both immediately before and after the star,
+they are the same.
+ */
+public class SameStarChar {
+
+  public boolean sameStarChar(String str) {
+
+    char[] chars = str.toCharArray();
+
+    for (int i = 0; i < str.length() - 3; i++) {
+      char firstChar = 
+    }
+
+  }
+
+
+}
