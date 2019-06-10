ac165ce841c35c14f8cd9c96760018013caec965
Felipe Cook <felipepcook@gmail.com>
2019-06-09 20:28:19 -0700

    Initial commit on gString Problem

---
+++ src/edu/cnm/deepdive/ProblemGString.java
@@ -0,0 +1,42 @@
+package edu.cnm.deepdive;
+
+public class ProblemGString {
+
+  public boolean gHappy(String str) {
+
+    if (str.length() <= 1) {
+      return true;
+    }
+    if ((str.length() == 2) && (str.charAt(str.length() -1) == 'g'
+        || str.charAt(str.length() - 1) == 'G')
+        && (str.charAt(str.length() - 2) == 'g'
+        || str.charAt(str.length() - 2)  == 'G')) {
+      return true;
+    }
+
+    boolean flag = false;
+
+    char[] chars = str.toCharArray();
+
+    for (int i = 0; i < str.length() - 2; i++) {
+      if (chars[i] == 'g' || chars[i] =='G') {
+        if (i == 0) {
+          if (chars[i + 1] == 'g' || chars[i + 1] == 'G') {
+            flag = true;
+          }
+        }
+
+        if ((chars[i + 1] == 'g' || chars[i + 1] == 'G') || (chars[i - 1] == 'g' || chars[i - 1] == 'G')) {
+          flag = true;
+        }
+
+      }
+
+    }
+    return flag;
+
+
+  }
+
+
+}
