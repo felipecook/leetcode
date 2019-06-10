11111cc030e3f06fefca1404c38650262efe7851
Felipe Cook <felipepcook@gmail.com>
2019-06-09 19:28:49 -0700

    Solved SameStarChar

--- src/edu/cnm/deepdive/SameStarChar.java
+++ src/edu/cnm/deepdive/SameStarChar.java
@@ -10,11 +10,38 @@ public class SameStarChar {
 
   public boolean sameStarChar(String str) {
 
+    if (str.length() <= 1) {
+      return true;
+    } else if (str.length() == 2) {
+      return true;
+    }
+
+    boolean flag = true;
+    int counter = 0;
+
+
     char[] chars = str.toCharArray();
 
-    for (int i = 0; i < str.length() - 3; i++) {
-      char firstChar = 
+    if (chars[str.length() - 1] == '*') {
+      return false;
     }
+
+
+    for (int i = 0; i < str.length() - 2; i++) {
+      char middleChar = chars[i + 1];
+
+      if (middleChar == '*' && i < str.length() - 1) {
+        if (chars[i] != chars[i + 2]) {
+          flag = false;
+        }
+        counter++;
+
+      }
+
+    }
+
+    return flag;
+
 
   }
 
---
+++ test/edu/cnm/deepdive/SameStarCharTest.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SameStarCharTest {
+
+  SameStarChar sameStarChar = new SameStarChar();
+
+  String string1 = ""
+
+  @Test
+  void sameStarChar() {
+
+    assertEquals(false, );
+
+
+  }
+}
