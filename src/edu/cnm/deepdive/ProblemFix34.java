fab27b73e9bb1e0111dbd2aacb1cb5c0acb8a983
Felipe Cook <felipepcook@gmail.com>
2019-06-10 20:25:47 -0700

    Initial Commit on Fix34

---
+++ src/edu/cnm/deepdive/ProblemFix34.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive;
+
+/*
+Return an array that contains exactly the same numbers as the
+given array, but rearranged so that every 3 is immediately
+followed by a 4. Do not move the 3's, but every other number
+may move. The array contains the same number of 3's and 4's,
+every 3 has a number after it that is not a 3, and a 3 appears
+in the array before any 4.
+ */
+
+public class ProblemFix34 {
+
+  public int[] fix34(int[] nums) {
+
+
+
+  }
+
+}
