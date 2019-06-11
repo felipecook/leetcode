76020ac912ed9fadea4964d162df433ae31cf284
Felipe Cook <felipepcook@gmail.com>
2019-06-10 20:10:23 -0700

    Solved Problem G

--- src/edu/cnm/deepdive/ProblemGString.java
+++ src/edu/cnm/deepdive/ProblemGString.java
@@ -16,26 +16,29 @@ public class ProblemGString {
         || str.charAt(str.length() - 2)  == 'G')) {
       return true;
     }
-    if (str.charAt(str.length() - 1) == 'g' || str.charAt(str.length() - 1) == 'G') {
-      return false;
-    }
+
 
     boolean flag = false;
 
     char[] chars = str.toCharArray();
 
-    for (int i = 0; i < str.length() - 2; i++) {
+    for (int i = 0; i < str.length() - 1; i++) {
       if (chars[i] == 'g' || chars[i] =='G') {
-        if (i == 0) {
-          if (chars[i + 1] == 'g' || chars[i + 1] == 'G') {
-            flag = true;
-          }
-        }
+//        if (i == 0) {
+//          if (chars[i + 1] == 'g' || chars[i + 1] == 'G') {
+//            flag = true;
+//          }
+//        }
 
         if ((chars[i + 1] == 'g' || chars[i + 1] == 'G') || (chars[i - 1] == 'g' || chars[i - 1] == 'G')) {
           flag = true;
+        } else {
+          flag = false;
         }
 
+      }
+      if (i == str.length() - 2 && chars[i] != 'g' && chars[i + 1] == 'g') {
+        return false;
       }
 
     }
