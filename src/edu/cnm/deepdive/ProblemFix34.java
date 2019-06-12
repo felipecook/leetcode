930ee7808767bfc03acd39c9b4393501a724d9df
Felipe Cook <felipepcook@gmail.com>
2019-06-11 21:25:39 -0700

    Initial Commit on Triple Threat problem

--- src/edu/cnm/deepdive/ProblemFix34.java
+++ src/edu/cnm/deepdive/ProblemFix34.java
@@ -22,6 +22,8 @@ public class ProblemFix34 {
       }
     }
 
+    return nums;
+
   }
 
 }
---
+++ src/edu/cnm/deepdive/TripleThreat.java
@@ -0,0 +1,19 @@
+package edu.cnm.deepdive;
+
+
+/*
+We'll say that a "triple" in a string is a char
+appearing three times in a row. Return the number
+of triples in the given string. The triples may overlap.
+ */
+public class TripleThreat {
+
+
+  public int countTriple(String str) {
+
+
+    return 0;
+  }
+
+
+}
