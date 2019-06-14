7324afad2722a5cc6716da7cd25efb400c95bb20
Felipe Cook <felipepcook@gmail.com>
2019-06-13 21:30:04 -0700

    Initial Commit on Sum all digits in a string

--- src/edu/cnm/deepdive/ProblemFix34.java
+++ src/edu/cnm/deepdive/ProblemFix34.java
@@ -7,19 +7,20 @@ followed by a 4. Do not move the 3's, but every other number
 may move. The array contains the same number of 3's and 4's,
 every 3 has a number after it that is not a 3, and a 3 appears
 in the array before any 4.
- */
+*/
 
 public class ProblemFix34 {
 
   public int[] fix34(int[] nums) {
 
-    for (int i = 0; i < nums.length; i++) {
+    for (int i = 0; i < nums.length - 2; i++) {
+      int holderVarA = 0;
+      int holderVarB = 0;
       if (nums[i] == 3) {
-        int holderVar = nums[i + 1];
-        for (int j = i; j < nums.length; j++) {
-
-        }
+        holderVarA = nums[i + 1];
+        holderVarB = nums[i + 2];
       }
+
     }
 
     return nums;
