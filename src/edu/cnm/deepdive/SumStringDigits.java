35bae17a9fda554cf938c64b65484e1d50a1cf63
Felipe Cook <felipepcook@gmail.com>
2019-06-14 18:55:45 -0700

    Solved sum all digits in a string problem

--- src/edu/cnm/deepdive/SumStringDigits.java
+++ src/edu/cnm/deepdive/SumStringDigits.java
@@ -11,4 +11,20 @@ converts a string to an int.)
 
 public class SumStringDigits {
 
+  public int sumDigits(String str) {
+
+    int counter = 0;
+
+    for (int i = 0; i < str.length(); i++) {
+      char c = str.charAt(i);
+      if (Character.isDigit(c)) {
+        counter += Character.getNumericValue(c);
+      }
+    }
+
+    return counter;
+
+  }
+
+
 }
