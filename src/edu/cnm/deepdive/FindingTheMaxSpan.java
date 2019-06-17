11e49bf4f34f24181ef2365f1019a86a27f52277
Felipe Cook <felipepcook@gmail.com>
2019-06-17 12:46:31 -0700

    First attempt at solution.

--- src/edu/cnm/deepdive/FindingTheMaxSpan.java
+++ src/edu/cnm/deepdive/FindingTheMaxSpan.java
@@ -12,20 +12,27 @@ the largest span found in the given array.
 public class FindingTheMaxSpan {
 
   public int maxSpan(int[] nums) {
-    int counter = 0;
-    int counter2 = 0;
+    int counter = 1;
+    int counter2 = 1;
 
     // This for loop iterates through each element in the array
     for (int i = 0; i < nums.length; i++) {
       int currentVar = nums[i];
 
-      // This for loop starts at the end of the array and iterates back through all the rest of the elements in the array until it finds a 
+      // This for loop starts at the end of the array and iterates back through all the rest of the elements in the array until it finds a
       for (int j = nums.length; j > currentVar; j--) {
         if (nums[j] == currentVar ) {
-          counter = j - i;
+          counter += j - i;
+          break;
         }
       }
+
+      if (counter > counter2) {
+        counter2 = counter;
+      }
     }
+
+    return counter2;
 
   }
 
