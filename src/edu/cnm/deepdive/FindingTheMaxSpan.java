1d9b3f568d4cc63aee15871798872c5c183ca5f0
Felipe Cook <felipepcook@gmail.com>
2019-06-16 20:11:33 -0700

    Find the max span in a int array initial commit

---
+++ src/edu/cnm/deepdive/FindingTheMaxSpan.java
@@ -0,0 +1,27 @@
+package edu.cnm.deepdive;
+
+/*
+Consider the leftmost and righmost appearances
+of some value in an array. We'll say that the
+"span" is the number of elements between the two
+inclusive. A single value has a span of 1. Returns
+the largest span found in the given array.
+(Efficiency is not a priority.)
+ */
+
+public class FindingTheMaxSpan {
+
+  public int maxSpan(int[] nums) {
+    int counter = 0;
+
+    for (int i = 0; i < nums.length - 1; i++) {
+      int currentVar = nums[i];
+
+      if (nums[i + 1] != currentVar)
+
+    }
+
+  }
+
+
+}
