77053e77ebbc9607dccda3cb98a6d781110c1b31
Felipe Cook <felipepcook@gmail.com>
2019-07-07 20:18:33 -0700

    Initial commit on codewars.com "Two to one"

--- src/edu/cnm/deepdive/LeetCodeProblems.java
+++ src/edu/cnm/deepdive/LeetCodeProblems.java
@@ -881,6 +881,14 @@ the largest span found in the given array.
 
   }
 
+  /*
+  Take 2 strings s1 and s2 including only letters from ato z.
+  Return a new sorted string, the longest possible, containing distinct letters,
+  each taken only once - coming from s1 or s2.
+   */
+  public String longest (String s1, String s2) {
+    
+  }
 
 }
 
