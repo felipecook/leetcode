f6592ad8fb5460c7761ecd0583db61992f37a5e9
Felipe Cook <felipepcook@gmail.com>
2019-07-19 20:15:46 -0700

    Initial commit on tenRun

--- src/edu/cnm/deepdive/LeetCodeProblems.java
+++ src/edu/cnm/deepdive/LeetCodeProblems.java
@@ -1072,6 +1072,18 @@ the largest span found in the given array.
 
   }
 
+  /*
+  For each multiple of 10 in the given array, change all the values
+  following it to be that multiple of 10, until encountering another
+  multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
+   */
+  public int[] tenRun(int[] nums) {
+
+
+
+  }
+
+
 
 }
 
